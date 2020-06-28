d1a4b71fc4db2f87b88a0bf774a0bd20420009c2
salih <salihzeki@gmail.com>
2020-06-28 16:24:29 -0400

    first change

--- hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World from Call'
+    return 'Hello World from SK'
 
 if __name__=='__main__':
     # app.run('localhost', port=5000, debug=True)
